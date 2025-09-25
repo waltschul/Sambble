e169f30467dca8cf607b938a24b55fc4eec2dcd1
Will Altschul <willaltschul@gmail.com>
2025-09-25 15:12:53 -0400

    Initial Commit

---
+++ Sambble/SambbleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SambbleApp.swift
+//  Sambble
+//
+//  Created by will on 9/25/25.
+//
+
+import SwiftUI
+
+@main
+struct SambbleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
